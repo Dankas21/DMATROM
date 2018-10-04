f3dcab70a8e59857a6bc9bc6e13420648d804d7f
Dankas21 <43099443+Dankas21@users.noreply.github.com>
2018-10-04 20:01:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br>
+	    <head>
+	    <meta charset="UTF-08>
+<title> Projeto Interdisciplinar</title>
+	   
+	
+	
+</head>
+<body>
 
+<?php
+	$NUM
 	echo "Olá Mundo 456!"
 	echo "olá Udemy e Hcode"
 
 
 	?>
+
+
+</body>
+	    
+	    
+	    
+	    
+	    
+
+	    
+	    
+	    
+	    </html>
+
+
+
+
